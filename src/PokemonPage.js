0fcbc4ec10ca25a58e1b1400ab86cc88a4baf104
Lawrence Obote <dev.obote@gmail.com>
2020-04-22 12:34:29 -0400

    Added Pokemon Page

---
+++ src/PokemonPage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class PokemonPage extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <h1>Pokemon<br/>Name</h1>
+                </div>
+                <div>
+                    <h2>Type</h2>
+                    <img>Sprite</img>
+                    <h2>Weight</h2>
+                </div>
+                
+            </div>
+        )
+    }
+}
