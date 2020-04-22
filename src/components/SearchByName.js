8b186cb7fdc4bccd9b4533762e6569ba7bd70c32
Lawrence Obote <dev.obote@gmail.com>
2020-04-22 12:06:42 -0400

    Worked on submit button

--- src/components/SearchByName.js
+++ src/components/SearchByName.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 class SearchByName extends Component {
+
+     storePokemon = async (e) => {
+        let pokemonName = document.getElementById('name').value;
+        console.log(pokemonName);
+    }
+    // goToPokemon = async (e) => {
+    //     try {
+    //         const pokemon = await get('https://pokeapi.co/api/v2/pokemon/' + pokemonName);
+    //     }
+    
+    // }
+
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={this.storePokemon}>
                     <label for="name">Enter Name</label>
-                    <input type="text" id="name" name="name" placeholder="Enter Pokemon Name"></input>
-                    <input type="submit" value="submit"></input>
+                    <input type="text" value={this.state.value} onChange={this.storePokemon} placeholder="Enter Pokemon Name"></input>
+                    <button type="submit" value="submit" onSubmit={this.storePokemon()}>Search By Name</button>
                 </form>
             </div>
         )
