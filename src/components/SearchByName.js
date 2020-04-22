a73bcfde48f78cd67d71a51ec4169330f1227f7f
Lawrence Obote <dev.obote@gmail.com>
2020-04-22 11:10:13 -0400

    Created SearchByName componenet

--- src/components/SearchByName.js
+++ src/components/SearchByName.js
@@ -4,7 +4,11 @@ class SearchByName extends Component {
     render() {
         return (
             <div>
-                
+                <form>
+                    <label for="name">Enter Name</label>
+                    <input type="text" id="name" name="name" placeholder="Enter Pokemon Name"></input>
+                    <input type="submit" value="submit"></input>
+                </form>
             </div>
         )
     }
