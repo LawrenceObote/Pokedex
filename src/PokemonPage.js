7b6cc8bd48932b656704d3519d87eb96ccd9c817
Lawrence Obote <dev.obote@gmail.com>
2020-04-22 13:09:52 -0400

    commented out image temporarily

--- src/PokemonPage.js
+++ src/PokemonPage.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react'
 
 export default class PokemonPage extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    state = {
+        
+    }
+
     render() {
         return (
             <div>
@@ -9,7 +17,7 @@ export default class PokemonPage extends Component {
                 </div>
                 <div>
                     <h2>Type: </h2>
-                    <img>Sprite</img>
+                    {/* <img>Sprite</img> */}
                     <h2>Weight: </h2>
                 </div>
                 <div>
