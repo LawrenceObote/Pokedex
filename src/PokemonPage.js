c1f7fb26055bf34393e0868d6b6a7f2a1b15d9b1
Lawrence Obote <dev.obote@gmail.com>
2020-04-22 12:37:23 -0400

    Added to UI

--- src/PokemonPage.js
+++ src/PokemonPage.js
@@ -8,9 +8,14 @@ export default class PokemonPage extends Component {
                     <h1>Pokemon<br/>Name</h1>
                 </div>
                 <div>
-                    <h2>Type</h2>
+                    <h2>Type: </h2>
                     <img>Sprite</img>
-                    <h2>Weight</h2>
+                    <h2>Weight: </h2>
+                </div>
+                <div>
+                    <h2>Order Number: </h2>
+                    <h2>Stats: </h2>
+                    <h2>Height: </h2>
                 </div>
                 
             </div>
