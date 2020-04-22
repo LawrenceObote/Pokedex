7261d6cf1c489d9204c140253c12d84d168e5941
Lawrence Obote <dev.obote@gmail.com>
2020-04-22 15:24:07 -0400

    worked on axios call

--- src/components/SearchByName.js
+++ src/components/SearchByName.js
@@ -9,11 +9,35 @@ class SearchByName extends Component {
     state = {
         value: ""
     }
+    
 
-     storePokemon = async (event) => {
+    storePokemon = async (event) => {
         this.setState({value: event.target.value});
         let pokemonName = this.state.value;
-        console.log(pokemonName)
+        return pokemonName;
+    }
+
+    getData = async () => {
+        try {
+        const pokemon = await axios.get('https://pokeapi.co/api/v2/pokemon/bulbasaur/')
+
+        console.log("aaaaa", pokemon);
+        this.setState({
+            name: pokemon.data.species.name,
+            type: pokemon.data.types[0],
+            type2: pokemon.data.types[1]
+        })
+        console.log(this.state.name);
+        console.log(this.state.type);
+        console.log(this.state.type2);
+        } catch(e) {
+            console.error(e)
+        }
+        
+    } 
+
+    componentDidMount() {
+        this.getData()
     }
 
     // onSubmit = (pokemonName) => {
