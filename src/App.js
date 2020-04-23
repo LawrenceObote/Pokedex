3caa602e716b34dfbe388d7b76a53aa65ee8591d
Lawrence Obote <dev.obote@gmail.com>
2020-04-22 21:31:31 -0400

    Did a lot of good stuff

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Route, Switch, Link , BrowserRouter as Router} from "react-router-dom";
 import './App.css';
 import SearchByName from './components/SearchByName'
 import PokemonPage from './PokemonPage'
+import axios from 'axios'
 
 class App extends Component {
   constructor(props) {
@@ -12,6 +13,34 @@ state = {
   name: "",
   value: ""
 }
+
+
+getData = async (e, value) => {
+  try {
+  const pokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${value}`)
+      console.log(pokemon.data.species.name);
+  this.setState({
+      name: pokemon.data.species.name,
+      type: pokemon.data.types[0].type.name,
+      type2: pokemon.data.types[1].type.name,
+      id: pokemon.data.id,
+      speed: pokemon.data.stats[0].base_stat,
+      specialDefense: pokemon.data.stats[1].base_stat,
+      specialAttack: pokemon.data.stats[2].base_stat,
+      defense: pokemon.data.stats[3].base_stat,
+      attack: pokemon.data.stats[4].base_stat,
+      hp: pokemon.data.stats[5].base_stat,
+      height: pokemon.data.height,
+      weight: pokemon.data.weight
+
+  })
+  } catch(e) {
+      console.error(e)
+  }
+  
+
+  
+} 
 
 
   render() {
