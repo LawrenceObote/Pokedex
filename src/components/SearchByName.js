084e5d4e9e8571f2098cfcb11e64091d48028ed4
Lawrence Obote <dev.obote@gmail.com>
2020-04-22 21:33:56 -0400

    added working search feature

--- src/components/SearchByName.js
+++ src/components/SearchByName.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios';
+import PokemonPage from '../PokemonPage'
 
 class SearchByName extends Component {
     constructor(props){
@@ -11,16 +12,12 @@ class SearchByName extends Component {
     }
     
 
-    storePokemon = async (event) => {
-        this.setState({value: event.target.value});
-        let pokemonName = this.state.value;
-        return pokemonName;
-    }
+   
 
-    getData = async () => {
+    getData = async (value) => {
         try {
-        const pokemon = await axios.get('https://pokeapi.co/api/v2/pokemon/bulbasaur/')
-
+        const pokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${value}/`)
+            
         this.setState({
             name: pokemon.data.species.name,
             type: pokemon.data.types[0].type.name,
@@ -43,7 +40,7 @@ class SearchByName extends Component {
     } 
 
     componentDidMount() {
-        this.getData()
+        
     }
 
     // onSubmit = (pokemonName) => {
@@ -56,14 +53,81 @@ class SearchByName extends Component {
     
     // }
 
+    search = async value => {
+        this.setState({loading: true});
+        const pokemon = await axios.get(
+            `https://pokeapi.co/api/v2/pokemon/${value}/`
+        );
+        console.log(pokemon);
+        if(pokemon.data.types[1] === undefined){
+        this.setState({
+            name: pokemon.data.species.name,
+            type: pokemon.data.types[0].type.name,
+            // type2: pokemon.data.types[1].type.name,
+            id: pokemon.data.id,
+            speed: pokemon.data.stats[0].base_stat,
+            specialDefense: pokemon.data.stats[1].base_stat,
+            specialAttack: pokemon.data.stats[2].base_stat,
+            defense: pokemon.data.stats[3].base_stat,
+            attack: pokemon.data.stats[4].base_stat,
+            hp: pokemon.data.stats[5].base_stat,
+            height: pokemon.data.height,
+            weight: pokemon.data.weight
+
+        })
+        } else {
+            this.setState({
+                name: pokemon.data.species.name,
+                type: pokemon.data.types[0].type.name,
+                type2: pokemon.data.types[1].type.name,
+                id: pokemon.data.id,
+                speed: pokemon.data.stats[0].base_stat,
+                specialDefense: pokemon.data.stats[1].base_stat,
+                specialAttack: pokemon.data.stats[2].base_stat,
+                defense: pokemon.data.stats[3].base_stat,
+                attack: pokemon.data.stats[4].base_stat,
+                hp: pokemon.data.stats[5].base_stat,
+                height: pokemon.data.height,
+                weight: pokemon.data.weight
+    
+            })
+        }
+        
+
+    };
+
+    onChangeHandler = async e => {
+        console.log(e.target.value);
+        this.search(e.target.value);
+        this.setState({value: e.target.value})
+    };
+
+    renderPokemon = () => {
+        let pokemon = <h1>There's no pokemon</h1>
+        if(this.state.pokemon) {
+            pokemon = <PokemonPage list={this.state.pokemon.name}/>
+        }
+
+        return pokemon;
+    }
+
+    
+
     render() {
         return (
             <div>
                 <form onSubmit={this.storePokemon}>
-                    <label htmlFor="name">Enter Name</label>
-                    <input type="text" value={this.state.value} onChange={this.storePokemon} placeholder="Enter Pokemon Name"></input>
-                    <button type="submit" value="submit">Search By Name</button>
+                    {/* <label htmlFor="name">Enter Name</label>
+                    <input type="text" value={this.state.value} onChange={this.storePokemon} placeholder="Enter Pokemon Name"></input> */}
+                    <button type="submit" value="submit" onSubmit={this.getData}>Search By Name</button>
+                    <button type="submit" onSubmit={this.getData}></button>
                 </form>
+                <input
+                value={this.state.value}
+                onChange={e => this.onChangeHandler(e)}
+                placeholder="Enter Pokemon"
+                />
+                {this.renderPokemon}
             </div>
         )
     }
