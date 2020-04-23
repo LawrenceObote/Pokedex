cd2175f845b88b0e3a2172539c53678d23d56d73
Lawrence Obote <dev.obote@gmail.com>
2020-04-23 12:00:27 -0400

    Added props to render method

--- src/components/SearchByName.js
+++ src/components/SearchByName.js
@@ -14,34 +14,31 @@ class SearchByName extends Component {
 
    
 
-    getData = async (value) => {
-        try {
-        const pokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${value}/`)
+    // getData = async (value) => {
+    //     try {
+    //     const pokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${value}/`)
             
-        this.setState({
-            name: pokemon.data.species.name,
-            type: pokemon.data.types[0].type.name,
-            type2: pokemon.data.types[1].type.name,
-            id: pokemon.data.id,
-            speed: pokemon.data.stats[0].base_stat,
-            specialDefense: pokemon.data.stats[1].base_stat,
-            specialAttack: pokemon.data.stats[2].base_stat,
-            defense: pokemon.data.stats[3].base_stat,
-            attack: pokemon.data.stats[4].base_stat,
-            hp: pokemon.data.stats[5].base_stat,
-            height: pokemon.data.height,
-            weight: pokemon.data.weight
+    //     this.setState({
+    //         name: pokemon.data.species.name,
+    //         type: pokemon.data.types[0].type.name,
+    //         type2: pokemon.data.types[1].type.name,
+    //         id: pokemon.data.id,
+    //         speed: pokemon.data.stats[0].base_stat,
+    //         specialDefense: pokemon.data.stats[1].base_stat,
+    //         specialAttack: pokemon.data.stats[2].base_stat,
+    //         defense: pokemon.data.stats[3].base_stat,
+    //         attack: pokemon.data.stats[4].base_stat,
+    //         hp: pokemon.data.stats[5].base_stat,
+    //         height: pokemon.data.height,
+    //         weight: pokemon.data.weight
 
-        })
-        } catch(e) {
-            console.error(e)
-        }
+    //     })
+    //     } catch(e) {
+    //         console.error(e)
+    //     }
         
-    } 
+    // } 
 
-    componentDidMount() {
-        
-    }
 
     // onSubmit = (pokemonName) => {
     //     <Redirect to={ '/${pokemonName}'} /> 
@@ -75,6 +72,7 @@ class SearchByName extends Component {
             weight: pokemon.data.weight
 
         })
+        
         } else {
             this.setState({
                 name: pokemon.data.species.name,
@@ -91,6 +89,7 @@ class SearchByName extends Component {
                 weight: pokemon.data.weight
     
             })
+            
         }
         
 
@@ -102,14 +101,27 @@ class SearchByName extends Component {
         this.setState({value: e.target.value})
     };
 
-    renderPokemon = () => {
-        let pokemon = <h1>There's no pokemon</h1>
-        if(this.state.pokemon) {
-            pokemon = <PokemonPage list={this.state.pokemon.name}/>
-        }
+    // renderPokemon = () => {
+    //     let pokemon = <h1>There's no pokemon</h1>
+    //     if(this.state.pokemon) {
+    //         pokemon = <PokemonPage 
+    //         name={this.state.name}
+    //         type={this.state.type}
+    //         type2={this.state.type2}
+    //         id={this.state.id}
+    //         speed={this.state.speed}
+    //         specialDefense={this.state.specialDefense}
+    //         specialAttack={this.state.specialAttack}
+    //         defense={this.state.defense}
+    //         attack={this.state.attack}
+    //         hp={this.state.hp}
+    //         height={this.state.height}
+    //         weight={this.state.weight}
+    //         />
+    //     }
 
-        return pokemon;
-    }
+    //     return pokemon;
+    // }
 
     
 
@@ -119,15 +131,28 @@ class SearchByName extends Component {
                 <form onSubmit={this.storePokemon}>
                     {/* <label htmlFor="name">Enter Name</label>
                     <input type="text" value={this.state.value} onChange={this.storePokemon} placeholder="Enter Pokemon Name"></input> */}
-                    <button type="submit" value="submit" onSubmit={this.getData}>Search By Name</button>
-                    <button type="submit" onSubmit={this.getData}></button>
+                    {/* <button type="submit" value="submit" onSubmit={this.getData}>Search By Name</button>
+                    <button type="submit" onSubmit={this.getData}></button> */}
                 </form>
                 <input
                 value={this.state.value}
                 onChange={e => this.onChangeHandler(e)}
                 placeholder="Enter Pokemon"
                 />
-                {this.renderPokemon}
+                {/* {this.renderPokemon} */}
+                <PokemonPage
+                name={this.state.name}
+                type={this.state.type}
+                type2={this.state.type2}
+                id={this.state.id}
+                speed={this.state.speed}
+                specialDefense={this.state.specialDefense}
+                specialAttack={this.state.specialAttack}
+                defense={this.state.defense}
+                attack={this.state.attack}
+                hp={this.state.hp}
+                height={this.state.height}
+                weight={this.state.weight} />
             </div>
         )
     }
