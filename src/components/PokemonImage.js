5dbc75eed58e70af3665115b4672367dbbeefba2
Lawrence Obote <dev.obote@gmail.com>
2020-04-24 13:39:44 -0400

    cleaned up code

---
+++ src/components/PokemonImage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+ function PokemonImage(props) {
+    
+    return (
+        <div>
+            
+            <img src={props.image}></img>
+            
+        </div>
+    )
+    
+}
+export default PokemonImage;
