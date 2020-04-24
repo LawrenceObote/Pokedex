5cd4a1174491940035da51af350886e14916d01b
Lawrence Obote <dev.obote@gmail.com>
2020-04-24 14:23:47 -0400

    added links to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
     return (
-        <div>
+        <div id="nav">
             <Link  id="link-to-home" to="/">Home</Link>
             <Link id="link-to-pokemon-page" to="/pokemon">Pokedex</Link>
             <Link id="link-to-more-info" to="/pokemon2">More Info</Link>
