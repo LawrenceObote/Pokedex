7e829be3341a7116021153448d47f46ec64da466
Lawrence Obote <dev.obote@gmail.com>
2020-04-24 13:37:45 -0400

    cleaned up code

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import {Route, Switch, Link , BrowserRouter as Router} from "react-router-dom";
+import { Link ,} from "react-router-dom";
 
 export default function Home() {
     return (
+
         <div id="welcome-div">
             <nav id="welcome">
                 <Link to="/pokemon">Welcome to Pokedex</Link>
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,23 +1,8 @@
-import React, { Component } from 'react'
-import axios from 'axios';
-import SearchByNumber from './SearchByNumber'
-import SearchByName from './SearchByName'
-import {Route, Switch, Link , BrowserRouter as Router} from "react-router-dom";
+import React from 'react'
+import PokemonImage from './PokemonImage'
 
-export default class PokemonPage extends Component {
-    constructor(props){
-        super(props)
-    }
+export default function PokemonPage(props) {
 
-    state = {
-        name: "",
-        type: "",
-        image: "",
-        Weight: "",
-        orderNumber: 0,
-        stats: "",
-        height: ""
-    }
 
     // getData = async () => {
     //     try {
@@ -29,29 +14,28 @@ export default class PokemonPage extends Component {
     // } 
 
 
-    render() {
-        console.log(this.props);
+    
         return (
             <div className="pokedex">
                 <div>
-                    <h1>Pokemon: <br/>{this.props.name}</h1>
+                    <h1>Pokemon: <br/>{props.name}</h1>
                 </div>
                 <div>
-                    <h2>Type: {this.props.type}</h2>
-                    <h2>Type2: {this.props.type2}</h2>
-                    <h2>Weight: {this.props.weight}</h2>
+                    <h2>Type: {props.type}</h2>
+                    <h2>Type2: {props.type2}</h2>
+                    <h2>Weight: {props.weight}</h2>
                 </div>
                 <div>
-                    <h2>Order Number: {this.props.id}</h2>
+                    <h2>Order Number: {props.id}</h2>
                     <h2>Stats: </h2>
-                    <h2>Height: {this.props.height}</h2>
-                    <img src={this.props.image}></img>
+                    <h2>Height: {props.height}</h2>
                 </div>
+                <PokemonImage />
 
                 
                 
                 
             </div>
         )
-    }
+    
 }
