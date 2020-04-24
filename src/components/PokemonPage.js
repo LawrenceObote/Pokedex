97193d78eaa140614e967d3b6af2ad6813818a3d
Lawrence Obote <dev.obote@gmail.com>
2020-04-24 15:24:39 -0400

    added name to pokemon page

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -18,7 +18,7 @@ export default function PokemonPage(props) {
         return (
             <div className="pokedex">
                 <div id="text">
-                    <h1>Pokemon: <br/>{props.name}</h1>
+                    <h1>Pokemon Name: <br/>{props.name}</h1>
                     <h4>Type: {props.type}</h4>
                     <h4>Type2: {props.type2}</h4>
                     <h4>Weight: {props.weight}</h4>
