be326edfede9ebee90a9174c84f8508fd63a38c8
Lawrence Obote <dev.obote@gmail.com>
2020-04-24 15:16:10 -0400

    added images to state

--- src/components/SearchByNumber.js
+++ src/components/SearchByNumber.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import { Link } from "react-router-dom";
 import AdditionalInfo from './AdditionalInfo'
+import PokemonImage from './PokemonImage'
 
 
 class SearchByNumber extends Component {
@@ -33,7 +34,8 @@ class SearchByNumber extends Component {
             attack: pokemon.data.stats[4].base_stat,
             hp: pokemon.data.stats[5].base_stat,
             height: pokemon.data.height,
-            weight: pokemon.data.weight
+            weight: pokemon.data.weight,
+            image: pokemon.data.sprites.front_default
 
         })
         
@@ -50,7 +52,8 @@ class SearchByNumber extends Component {
                 attack: pokemon.data.stats[4].base_stat,
                 hp: pokemon.data.stats[5].base_stat,
                 height: pokemon.data.height,
-                weight: pokemon.data.weight
+                weight: pokemon.data.weight,
+                image: pokemon.data.sprites.front_default
     
             })
             
@@ -63,7 +66,7 @@ class SearchByNumber extends Component {
     onChangeHandler = async e => {
         console.log(e.target.value);
         this.search(e.target.value);
-        this.setState({value: e.target.value})
+        this.setState({value: e.target.value.toLowerCase()})
     };
 
 
@@ -73,25 +76,31 @@ class SearchByNumber extends Component {
         return (
             <div>
 
-                <input id="search"
-                value={this.state.value}
-                onChange={e => this.onChangeHandler(e)}
-                placeholder="Enter Pokemon"
+                <input 
+                    id="search"
+                    value={this.state.value}
+                    onChange={e => this.onChangeHandler(e)}
+                    placeholder="Enter Pokemon"
                 />
                 
             
-            <AdditionalInfo 
-                name={this.state.name}
-                speed={this.state.speed}
-                specialDefense={this.state.specialDefense}
-                specialAttack={this.state.specialAttack}
-                defense={this.state.defense}
-                attack={this.state.attack}
-                hp={this.state.hp}
-                image={this.state.image}/>
+                <AdditionalInfo 
+                    name={this.state.name}
+                    speed={this.state.speed}
+                    specialDefense={this.state.specialDefense}
+                    specialAttack={this.state.specialAttack}
+                    defense={this.state.defense}
+                    attack={this.state.attack}
+                    hp={this.state.hp}
+                    image={this.state.image}
+                />
 
-                    <button id="previous" onClick={this.handlePreviousPokemonButton}>Previous Pokemon</button>
-                    <button id="next" onClick={this.handleNextPokemonButton}>Next Pokemon</button>
+                <PokemonImage id="image"
+                    image={this.state.image}
+                 />
+
+                <button id="previous" onClick={this.handlePreviousPokemonButton}>Previous Pokemon</button>
+                <button id="next" onClick={this.handleNextPokemonButton}>Next Pokemon</button>
             </div>
         )
     }
