58ed0598cd5c34cb848ffba114f5eb9c842460fc
Lawrence Obote <dev.obote@gmail.com>
2020-04-24 20:42:46 -0400

    comments

--- src/components/AdditionalInfo.js
+++ src/components/AdditionalInfo.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
-
+//Setting up a functional component to display additional information
 export default function AdditionalInfo(props) {
     return (
+        //Parent div with a classname of Pokedex to hold all of the info
         <div className="pokedex">
+            {/* div with a class name of text to style the text */}
             <div id="text">
+            {/* displaying all of the props that have been passed from the stateful components */}
             <h2>name: {props.name}</h2>
             <h3>speed: {props.speed}</h3>
             <h3>Special Defense: {props.specialDefense}</h3>
