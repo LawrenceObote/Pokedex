094c11b39fc690cba445e377a2ae7fab77a060ba
Lawrence Obote <dev.obote@gmail.com>
2020-04-24 20:45:24 -0400

    comments

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,23 +1,15 @@
 import React from 'react'
-import PokemonImage from './PokemonImage'
+
 
 export default function PokemonPage(props) {
 
-
-    // getData = async () => {
-    //     try {
-    //         const pokemon = await axios.get('https://pokeapi.co/api/v2/pokemon/');
-    //         console.log(pokemon.data);
-    //     } catch(e) {
-    //         console.error(e);
-    //     }
-    // } 
-
-
     
         return (
+            // creating a dig with the class name of pokedex
             <div className="pokedex">
+                {/* creating a div with the class name of text */}
                 <div id="text">
+                    {/* rendering the info and displaying props */}
                     <h1>Pokemon Name: <br/>{props.name}</h1>
                     <h4>Type: {props.type}</h4>
                     <h4>Type2: {props.type2}</h4>
