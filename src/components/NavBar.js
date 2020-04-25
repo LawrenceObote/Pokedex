bbc2063769b79e8f7b9dd67e3a83d53a1a356560
Lawrence Obote <dev.obote@gmail.com>
2020-04-24 20:43:47 -0400

    comments

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
     return (
+        //setting up a navbar and connecting each individual link to the appropriate page
         <div id="nav">
             <Link  id="link-to-home" to="/">Home</Link>
             <Link id="link-to-pokemon-page" to="/pokemon">Pokedex</Link>
