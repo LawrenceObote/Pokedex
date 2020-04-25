9461be6a37edda681c386f2dffb7aea41deaa6eb
Lawrence Obote <dev.obote@gmail.com>
2020-04-24 20:43:12 -0400

    comments

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,11 @@ import { Link ,} from "react-router-dom";
 
 export default function Home() {
     return (
-
+        //setting parent div for welcome page
         <div id="welcome-div">
+            {/* setting a link that navigates to the main page */}
             <nav id="welcome">
+                {/* link to take user to the main page */}
                 <Link to="/pokemon">Welcome to Pokedex</Link>
             </nav>
             
