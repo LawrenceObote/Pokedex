61362c97abbe3162c1b375f04864970cf30e1790
Lawrence Obote <dev.obote@gmail.com>
2020-04-24 20:46:29 -0400

    comments

--- src/components/SearchByNumber.js
+++ src/components/SearchByNumber.js
@@ -6,23 +6,22 @@ import Button from 'react-bootstrap/Button';
 
 
 class SearchByNumber extends Component {
-    constructor(props){
-        super(props)
-    }
-
+    //setting state. The value component will be used to search through the API
     state = {
-        value: "",
-        toPokemon: null
+        value: ""
     }
 
+    //Asynchronous search function. Pulls API data then stores it in state
     search = async value => {
-        
+        //storing API data in a variable
         const pokemon = await axios.get(
             `https://pokeapi.co/api/v2/pokemon/${value}/`
         );
-        console.log(pokemon);
+        //Not all pokemon have a second type. The If statement adjusts the data depnding on if they have a second type or not
         if(pokemon.data.types[1] === undefined){
+            //storing each piece of data in state.
         this.setState({
+            
             name: pokemon.data.species.name,
             type: pokemon.data.types[0].type.name,
             type2: "",
@@ -61,27 +60,36 @@ class SearchByNumber extends Component {
         
 
     };
-
-
-    onChangeHandler = async e => {
+    //Handles the input field text and runs the search function to update state to re render the dom.
+    //Then state is updated to make sure that the dom renders with every key stroke.
+    onChangeHandler = e => {
         console.log(e.target.value);
         this.search(e.target.value);
         this.setState({value: e.target.value.toLowerCase()})
     };
 
-
+    //Function that handles the previous pokemon press.
     handlePreviousPokemonButton = () => {
+        //stores state in a variable
         let counter = this.state.id;
+        //sets the variable to 1 less than state.
         counter = this.state.id - 1;
+        //runs the search method with state - 1.
         this.search(counter)
+        //updates the state value property to 1 less than the previous state.
         this.setState({ value: counter});
         
     }
 
+    //Function that handles the previous pokemon press.
     handleNextPokemonButton = () => {
+        //stores state in a variable
         let counter = this.state.id;
+        //sets the variable to 1 more than state.
         counter = this.state.id + 1;
+        //runs the search method with state + 1.
         this.search(counter);
+        //updates the state value property to 1 more than the previous state
         this.setState({ value: counter});
         
     }
@@ -92,7 +100,7 @@ class SearchByNumber extends Component {
         
         return (
             <div>
-
+                {/* Setting up input field for the search bar */}
                 <input 
                     id="search"
                     value={this.state.value}
@@ -100,7 +108,7 @@ class SearchByNumber extends Component {
                     placeholder="Enter Pokemon"
                 />
                 
-            
+            {/* render the aditional info on the page. pass it as props */}
                 <AdditionalInfo 
                     name={this.state.name}
                     speed={this.state.speed}
@@ -112,10 +120,11 @@ class SearchByNumber extends Component {
                     image={this.state.image}
                 />
 
+                {/* Displays pokemon image on the page. passing it as props */}
                 <PokemonImage id="image"
                     image={this.state.image}
                  />
-
+                {/* Buttons that move the id to previous and next pokemon */}
                 <Button variant="dark" className="previous" onClick={this.handlePreviousPokemonButton}>Previous</Button> 
                 <Button variant="dark"  className="next" onClick={this.handleNextPokemonButton}>Next</Button>
             </div>
